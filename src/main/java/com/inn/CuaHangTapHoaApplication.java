9a4e9bca494901e1702f23de6034b7763aba528f
Hoang <trunghoangdz134@gmail.com>
2025-10-24 22:07:55 +0700

    first commit

---
+++ src/main/java/com/inn/CuaHangTapHoaApplication.java
@@ -0,0 +1,13 @@
+package com.inn.taphoa.com.inn.taphoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuaHangTapHoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuaHangTapHoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/CuaHangTapHoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.taphoa.com.inn.taphoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuaHangTapHoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
