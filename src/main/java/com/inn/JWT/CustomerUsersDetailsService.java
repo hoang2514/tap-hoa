e83dd20e7688e1fb246a5f0054e08791f420f6d0
Hoang <trunghoangdz134@gmail.com>
2025-11-07 07:38:04 +0700

    Add Spring Security configuration and custom user details service

---
+++ src/main/java/com/inn/JWT/CustomerUsersDetailsService.java
@@ -0,0 +1,38 @@
+package com.inn.JWT;
+
+import com.inn.dao.UserDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class CustomerUsersDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    private com.inn.POJO.User userDetail;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Inside loadUserByUsername {}", username);
+        userDetail = userDao.findByEmail(username);
+        if(!Objects.isNull(userDetail)) {
+            return new User(userDetail.getEmail(), userDetail.getPassword(), new ArrayList<>());
+        }
+        else
+            throw new UsernameNotFoundException("User not found.");
+    }
+
+    public com.inn.POJO.User getUserDetail() {
+        return userDetail;
+    }
+}
