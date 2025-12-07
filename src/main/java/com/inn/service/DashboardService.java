5a506184dcf86b6b437977f664cc0e9ce46c4d62
Hoang <trunghoangdz134@gmail.com>
2025-12-07 12:07:19 +0700

    add api dashboard

---
+++ src/main/java/com/inn/service/DashboardService.java
@@ -0,0 +1,8 @@
+package com.inn.service;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface DashboardService {
+    ResponseEntity<Map<String, Object>> getCount();
+}
