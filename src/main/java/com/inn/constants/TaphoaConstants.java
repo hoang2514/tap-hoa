ed40b9d88342b7980a52d3162284c0b68626b87b
Hung Nguyen <nguyenlequochung3011@gmail.com>
2025-12-17 10:55:14 +0700

    fix at bills

--- src/main/java/com/inn/constants/TaphoaConstants.java
+++ src/main/java/com/inn/constants/TaphoaConstants.java
@@ -4,5 +4,5 @@ public class TaphoaConstants {
     public static final String Something_Went_Wrong = "Something Went Wrong";
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized Access";
     public static final String INVALID_DATA = "Invalid Data";
-    public static final String STORE_LOCATION = "D:\\prj_web";
+    public static final String STORE_LOCATION = "C:\\Users\\Administrator\\Documents";
 }
