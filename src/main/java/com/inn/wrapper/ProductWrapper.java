3cbbb47bad17322d4e07534ffd073cf5d035034d
Edumiz <huyhoang.26062004@gmail.com>
2025-12-27 13:00:42 +0700

    fixing bug in ProductWrapper

--- src/main/java/com/inn/wrapper/ProductWrapper.java
+++ src/main/java/com/inn/wrapper/ProductWrapper.java
@@ -7,15 +7,15 @@ import lombok.Data;
 // Implements Serializable for storing in Redis
 public class ProductWrapper implements Serializable {
 
-    Integer id;
-    String name;
-    String description;
-    Float price;
-    String status;
-    Integer categoryId;
-    String categoryName;
-    Integer quantity;
-    String imageUrl;
+    private Integer id;
+    private String name;
+    private String description;
+    private Float price;
+    private String status;
+    private Integer quantity;
+    private Integer categoryId;
+    private String categoryName;
+    private String imageUrl;
 
     public ProductWrapper() {
 
@@ -41,15 +41,15 @@ public class ProductWrapper implements Serializable {
         this.quantity = quantity;
     }
 
-    public ProductWrapper(Integer id, String name, String description, Float price, String status, Integer categoryId, String categoryName, Integer quantity, String imageUrl) {
+    public ProductWrapper(Integer id, String name, String description, Float price, String status, Integer quantity, Integer categoryId, String categoryName, String imageUrl) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.status = status;
-        this.imageUrl = imageUrl;
+        this.quantity = quantity;  
         this.categoryId = categoryId;
         this.categoryName = categoryName;
-        this.quantity = quantity;
+        this.imageUrl = imageUrl;
     }
 }
